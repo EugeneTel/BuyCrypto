c919262b43417f0a068202f86dbec28d44581187
ginteam <kasyanchukigor@gmail.com>
2017-07-31 09:35:59 +0300

    CrypCarrency model added for ikas.parser plugin

---
+++ plugins/ikas/parser/models/CrypCurrency.php
@@ -0,0 +1,37 @@
+<?php namespace Ikas\Parser\Models;
+
+use Model;
+
+/**
+ * CrypCurrency Model
+ */
+class CrypCurrency extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'ikas_parser_cryp_currencies';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $hasOne = [];
+    public $hasMany = [];
+    public $belongsTo = [];
+    public $belongsToMany = [];
+    public $morphTo = [];
+    public $morphOne = [];
+    public $morphMany = [];
+    public $attachOne = [];
+    public $attachMany = [];
+}
